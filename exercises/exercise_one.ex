3ebca5a03e8122b9b7d1ccabda48db8223b07a81
Malcolm Peter <kraftycoder@gmail.com>
2018-10-19 06:41:04 +0300

    add exercise one with it's solution

--- exercises/exercise_one.ex
+++ exercises/exercise_one.ex
@@ -1,12 +1,12 @@
 # Which of the following will match?
 
-# 1 – a = [1, 2, 3]
-# 2 – a=4
-# 3 – 4=a
-# 4 – [a, b] = [ 1, 2, 3 ]
-# 5 – a = [ [ 1, 2, 3 ] ]
-# 6 – [a] = [ [ 1, 2, 3 ] ]
-# 7 – [[a]] = [ [ 1, 2, 3 ] ]
+# 1 – a = [1, 2, 3]               # will match
+# 2 – a=4                         # will match
+# 3 – 4=a                         # will match
+# 4 – [a, b] = [ 1, 2, 3 ]        # will not match
+# 5 – a = [ [ 1, 2, 3 ] ]         # will match
+# 6 – [a] = [ [ 1, 2, 3 ] ]       # will match
+# 7 – [[a]] = [ [ 1, 2, 3 ] ]     # will not match
 
 # => [1, 2, 3, 5, 6]
 
