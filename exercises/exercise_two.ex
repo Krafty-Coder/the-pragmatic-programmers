1e056ef1ade8943f69c74635dc28fcf177dc5010
Malcolm Peter <kraftycoder@gmail.com>
2018-10-20 06:41:58 +0300

    add exercise two's solution

--- exercises/exercise_two.ex
+++ exercises/exercise_two.ex
@@ -1,14 +1,21 @@
 # Which of the fiollowing will match?
-# – [ a, b, a ] = [ 1, 2, 3 ]
-# – [ a, b, a ] = [ 1, 1, 2 ]
-# – [ a, b, a ] = [ 1, 2, 1 ]
+# 1 – [ a, b, a ] = [ 1, 2, 3 ]
+# 2 – [ a, b, a ] = [ 1, 1, 2 ]
+# 3 – [ a, b, a ] = [ 1, 2, 1 ]
 # The variable a is bound to the value 2 . Which of the following will match?
-# – [ a, b, a ] = [ 1, 1, 2 ]
-# – [ a, b, a ] = [ 1, 2, 3 ]
-# – a = 1
-# – ^a = 2
-# – ^a = 1
-# – ^a = 2 - a
+# 4 – [ a, b, a ] = [ 1, 1, 2 ]
+# 5 – [ a, b, a ] = [ 1, 2, 3 ]
+# 6 – a = 1
+# 7 – ^a = 2
+# 8 – ^a = 1
+# 9 – ^a = 2 - a
+
+# Answer ||
+#        \/
+
+
+      # 3, 6, 7
+
 
 
 
