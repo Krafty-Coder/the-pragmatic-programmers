83593586ff55c31e44592cc3c9c64f704659d3bf
Malcolm Peter <kraftycoder@gmail.com>
2018-10-22 06:42:42 +0300

    add exercise four's solution

--- exercises/exercise_four.ex
+++ exercises/exercise_four.ex
@@ -10,3 +10,8 @@
 # 14, FizzBuzz, 16.”
 # (Yes, it’s a FizzBuzz solution with no conditional logic.) 1
 
+whichfizz = fn (0, 0, _) -> "FizzBuzz" (0, _, _) -> "Fizz" (_, 0, _) -> "Buzz" end
+
+fizzbuzz = fn n -> whichfizz.(rem(n, 3), rem(n, 5), n) end
+
+IO.inspect Enum.map(1..100, fizzbuzz)
