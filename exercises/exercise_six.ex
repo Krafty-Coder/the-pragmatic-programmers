cea83876776c20f400930f13c6b1435a6b8e71c9
Malcolm Peter <kraftycoder@gmail.com>
2018-10-22 06:43:10 +0300

    add exercise six's solution

--- exercises/exercise_six.ex
+++ exercises/exercise_six.ex
@@ -1,3 +1,9 @@
 # Use the & notation to rewrite the following:
 # – Enum.map [1,2,3,4], fn x -> x + 2 end
 # – Enum.each [1,2,3,4], fn x -> IO.inspect x end
+
+x = [1,2,3,4]
+
+add_two = Enum.map x, &(&1+2)
+
+inspect_each = Enum.map [1,2,3,4], &(IO.inspect(&1))
