5685650b4938c76d479719a048fdafb7f55b2226
Malcolm Peter <kraftycoder@gmail.com>
2018-10-23 06:43:45 +0300

    add exercise seven's solution

--- exercises/exercise_seven.ex
+++ exercises/exercise_seven.ex
@@ -7,3 +7,15 @@
 
 # ➤ Exercise: ModulesAndFunctions-3
 # Add a quadruple function. (Maybe it could call the double function....)
+
+defmodule Times do
+  def double(n) do
+    n * 2
+  end
+  def triple(n) do
+    n * 3
+  end
+  def quadruple(n) do
+    double(n)+double(n)
+  end
+end
