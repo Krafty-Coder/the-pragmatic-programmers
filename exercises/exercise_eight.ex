0d45f22ae07ef9c7af94d567fbb54d340b0a0500
Malcolm Peter <kraftycoder@gmail.com>
2018-10-24 06:44:15 +0300

    add exercise eight's solution

--- exercises/exercise_eight.ex
+++ exercises/exercise_eight.ex
@@ -3,7 +3,23 @@
 # sum of the integers from 1 to n. You’ll need to write this function inside
 # a module in a separate file. Then load up IEx, compile that file, and try
 # your function.
+
 # ➤ Exercise: ModulesAndFunctions-5
 # Write a function gcd(x,y) that finds the greatest common divisor between
 # two nonnegative integers. Algebraically, gcd(x,y) is x if y is zero; it’s gcd(y,
 # rem(x,y)) otherwise.
+
+defmodule Math do
+  def sum(0) do
+    0
+  end
+  def sum(n) do
+    n + sum(n-1)
+  end
+  def gcd(x,0) do
+    x
+  end
+  def gcd(x,y) do
+    gcd(y, rem(x,y))
+  end
+end
