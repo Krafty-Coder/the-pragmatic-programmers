ac27b36418a184b3edd2ab37ff8dd6db8a4f7fb2
Malcolm Peter <kraftycoder@gmail.com>
2018-10-25 06:45:20 +0300

    add exercise twelve's solution

--- exercises/exercise_twelve.ex
+++ exercises/exercise_twelve.ex
@@ -1,3 +1,9 @@
 # ➤ Exercise: ListsAndRecursion-4
 # Write a function MyList.span(from, to) that returns a list of the numbers from
 # from up to to .
+
+defmodule MyList do
+  def span(a, b) do
+    Enum.to_list(a..b)
+  end
+end
